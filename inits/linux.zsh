80a0665b5575eac6a32bf5c20d29d0d6c8ff51f4
sankitch <i@sankitch.me>
2013-12-10 16:14:15 +0900

    Install pyenv.

--- inits/linux.zsh
+++ inits/linux.zsh
@@ -6,3 +6,6 @@ eval "$(rbenv init -)"
 if [[ -f ~/.nodebrew/nodebrew ]]; then
     nodebrew use latest
 fi
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
